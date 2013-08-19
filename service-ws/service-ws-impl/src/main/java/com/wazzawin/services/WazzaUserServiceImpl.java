08ec54c10026f8ccd6ddbbc067db65b40b2b33b5
andypower <nazzareno.sileno@geosdi.org>
2013-08-20 00:32:34 +0200

    Added GPL v3 + CE source code license

--- service-ws/service-ws-impl/src/main/java/com/wazzawin/services/WazzaUserServiceImpl.java
+++ service-ws/service-ws-impl/src/main/java/com/wazzawin/services/WazzaUserServiceImpl.java
@@ -1,3 +1,38 @@
+/*
+ *  wazza-services
+ *  WazzaWins Services
+ *  https://github.com/WazzaWin/wazza-services
+ * ====================================================================
+ *
+ * Copyright (C) 2012-2013 WazzaWin Developer Group (WazzaWin Team - Potenza - ITALY).
+ *
+ * This program is free software: you can redistribute it and/or modify it 
+ * under the terms of the GNU General Public License as published by 
+ * the Free Software Foundation, either version 3 of the License, or 
+ * (at your option) any later version. This program is distributed in the 
+ * hope that it will be useful, but WITHOUT ANY WARRANTY; without 
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR 
+ * A PARTICULAR PURPOSE. See the GNU General Public License 
+ * for more details. You should have received a copy of the GNU General 
+ * Public License along with this program. If not, see http://www.gnu.org/licenses/ 
+ *
+ * ====================================================================
+ *
+ * Linking this library statically or dynamically with other modules is 
+ * making a combined work based on this library. Thus, the terms and 
+ * conditions of the GNU General Public License cover the whole combination. 
+ * 
+ * As a special exception, the copyright holders of this library give you permission 
+ * to link this library with independent modules to produce an executable, regardless 
+ * of the license terms of these independent modules, and to copy and distribute 
+ * the resulting executable under terms of your choice, provided that you also meet, 
+ * for each linked independent module, the terms and conditions of the license of 
+ * that module. An independent module is a module which is not derived from or 
+ * based on this library. If you modify this library, you may extend this exception 
+ * to your version of the library, but you are not obligated to do so. If you do not 
+ * wish to do so, delete this exception statement from your version. 
+ *
+ */
 package com.wazzawin.services;
 
 import com.google.common.collect.Lists;
@@ -9,7 +44,6 @@ import com.wazzawin.exception.ResourceNotFoundFault;
 import com.wazzawin.responce.user.WazzaUserDTO;
 import com.wazzawin.service.WazzaWSRestService;
 import com.wazzawin.shared.utils.WazzaUtils;
-import java.util.Collections;
 import java.util.List;
 import org.dozer.DozerBeanMapper;
 import org.slf4j.Logger;
@@ -48,7 +82,7 @@ class WazzaUserServiceImpl {
     //</editor-fold>
 
     /**
-     * @see WazzaWSRestService#getSimpleUser(java.lang.String) 
+     * @see WazzaWSRestService#getSimpleUser(java.lang.String)
      */
     public WazzaUserDTO getUser(String userName) {
         logger.debug("Username to find: " + userName);
@@ -57,7 +91,7 @@ class WazzaUserServiceImpl {
     }
 
     /**
-     * @see WazzaWSRestService#getAllWazzaUsers() 
+     * @see WazzaWSRestService#getAllWazzaUsers()
      */
     public List<WazzaUserDTO> getAllUsers() {
         List<WazzaUser> wazzaUserList = wazzaUserDAO.findAll();
@@ -70,7 +104,7 @@ class WazzaUserServiceImpl {
 
     /**
      * @see
-     * WazzaWSRestService#insertWazzaUser(com.wazzawin.responce.user.WazzaUserDTO) 
+     * WazzaWSRestService#insertWazzaUser(com.wazzawin.responce.user.WazzaUserDTO)
      */
     public Long insertUser(WazzaUserDTO wazzaUserDTO)
             throws IllegalParameterFault {
@@ -192,12 +226,11 @@ class WazzaUserServiceImpl {
     }
 
     /**
-     * @see WazzaWSRestService#deleteWazzaUser(java.lang.Long) 
+     * @see WazzaWSRestService#deleteWazzaUser(java.lang.Long)
      */
     public void deleteUser(Long wazzaUserID) throws ResourceNotFoundFault {
         this.getWazzaUserById(wazzaUserID);
 //      TODO: Remove all the existing relations to the wazzaUser removed
         wazzaUserDAO.delete(wazzaUserID);
     }
-
 }
